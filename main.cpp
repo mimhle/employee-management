5fce49b7cb465c182dce84f0965e942de3124443
Nguyen Duc Tam <nguyndantdm6@gmail.com>
2023-11-14 19:27:27 +0700

    login

--- main.cpp
+++ main.cpp
@@ -1,45 +1,149 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
 #include "UserInterface.h"
+
+UserInterface;
+
+//read data.txt __
+bool authenticate(const std::string& username, const std::string& password) {
+    std::ifstream file("data.txt");
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening file.\n";
+        return false;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string storedUsername, storedPassword;
+
+        // Read the fields from the CSV line
+        if (std::getline(iss, storedUsername, ',') && std::getline(iss, storedPassword)) {
+            if (storedUsername == username && storedPassword == password) {
+                return true; // Authentication successful
+            }
+        }
+    }
+
+    return false; // Authentication failed
+}
+
+bool checkUsername(const std::string& username) {
+    std::ifstream file("data.txt");
+
+    if (!file.is_open()) {
+        return false;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string storedUsername;
+        if (std::getline(iss, storedUsername)) {
+            if (storedUsername == username) {
+                return true;
+            }
+        }
+    }
+}
 
 int main() {
     UserInterface ui;
-    ui.cBorder = '*';
-    
-    ui.printTitle("No cap!", YELLOW, CYAN);
-    ui.printLineBreak();
-    ui.printTitle("With cap!  " + std::to_string(1), YELLOW, CYAN, true);
-    
-    ui.printLineBreak();
-    ui.printLineBreak();
-    
-    ui.input("Username: ", LIGHT_BLUE, false);
-    std::string pass = ui.input("Password (default: 123): ", LIGHT_BLUE, true);
-    
-    if (pass == "123") {
-        ui.printTitle("Access granted!", GREEN, CYAN);
-    } else {
-        ui.printTitle("Access denied!", RED, CYAN);
-        system("pause");
-        return 0;
+    int cnt = 0;
+    std::string user;
+    std::string pass;
+    bool login = false;
+    //login
+    while (cnt != 4) {
+        ui.cBorder = '*';
+
+        ui.printTitle("Cong ty A", YELLOW, CYAN);
+        ui.printLineBreak();
+        ui.printTitle("Login Admin  ", YELLOW, CYAN, true);
+
+        ui.printLineBreak();
+        ui.printLineBreak();
+
+        user = ui.input("Username: ", LIGHT_BLUE, false);
+        pass = ui.input("Password (default: 123): ", LIGHT_BLUE, true);
+        login = authenticate(user, pass);
+        if (login) {
+            ui.printTitle("Access granted!", GREEN, CYAN);
+            break;
+        }
+        else if(cnt!=3){
+            ui.printTitle("You have been denied " + std::to_string(cnt + 1) + " please press Enter to continue!", RED, CYAN);
+            std::cin.get();
+            //system("pause");
+            ui.clearScreen();
+            cnt++;
+        }
+        else {
+            ui.printTitle("Access denied you have 4 time, please try again!", RED, CYAN);
+            std::cin.get();
+            break;
+        }
     }
+
     
-    ui.clearScreen();
-    
-    ui.printCentered("Menu", YELLOW, CYAN, '-');
-    ui.printMultiLine(
-        {
-            "1. Option 1",
-            "2. Option 2",
-            "3. Option 3",
-            "4. Option 4"
-        },
-        LIGHT_BLUE
-    );
-    
-    while (1 != 2) {
-        std::string s;
-        s = ui.input(">", LIGHT_YELLOW, false);
-        ui.print(s, LIGHT_BLUE, true);
+    //after_login
+    if (login) {
+        while (1 != 2) {
+            ui.clearScreen();
+            if (login) {
+                ui.printCentered("Menu", YELLOW, CYAN, '-');
+                ui.printMultiLine(
+                    {
+                        "1. Add employee",
+                        "2. Delete employee",
+                        "3. Find employee",
+                        "4. Edit employee",
+                        "5. Show employee information",
+                        "6. Return!"
+                    },
+                    LIGHT_BLUE
+                );
+
+
+            }
+            std::string s;
+            s = ui.input(">", LIGHT_YELLOW, false);
+            if (s == "1") {
+                ui.clearScreen();
+                std::ofstream file("data.txt", std::ios::app);
+                std::string _user = ui.input("UserName: ");
+                std::string _pass = ui.input("Password: ");
+                std::string _vpass = ui.input("Verify Password: ");
+                //check_xem_ten_co_ton_tai_khum
+                if (checkUsername(_user)) {
+                    ui.print("Username already exists, please press enter to return menu!!");
+                    std::cin.get();
+                }
+                else if (_pass != _vpass) {
+                    ui.print("Please verify your password again");
+                }
+                else {
+                    //file << _user << "," << _pass << "\n";
+                    ui.print("Sign up successfully");
+                }
+                file.close();
+
+            }
+            else if (s == "2") {
+                ui.clearScreen();
+                ui.printTitle("Xoa Nhan vien");
+                std::cin.get();
+            }
+            else if (s == "6") {
+                ui.clearScreen();
+                ui.printTitle("Thanks for using us", RED);
+                break;
+            }
+        }
     }
     return 0;
 }
