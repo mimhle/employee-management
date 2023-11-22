ae633cbf71ee5162ab0f313caeb766ce92dd9c4d
Tamnemng <119040174+Tamnemng@users.noreply.github.com>
2023-11-22 17:40:00 +0700

    add User.h

---
+++ Users.h
@@ -0,0 +1,69 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "modernize-use-nodiscard"
+#ifndef CTDL_GK_USERS_H
+#define CTDL_GK_USERS_H
+
+#include <string>
+#include <vector>
+#include "LinkedList.cpp"
+#include "UserData.h"
+#include "CsvFile.h"
+
+/**
+ * @class Users
+ * @brief Class for Users
+ * @details This class contains functions for Users
+ */
+class Users {
+private:
+    LinkedList<UserData> _list;
+public:
+    Users();
+
+    ~Users();
+
+    /**
+     * @brief Add user to list
+     * @param user User to add
+     * @return True if success, false if failed
+     */
+    bool addUser(const UserData& user);
+
+    /**
+     * @brief Remove user from list
+     * @param user User to remove
+     */
+    void removeUser(const UserData& user);
+
+    /**
+     * @brief Edit user in list
+     * @param userName Username of user to edit
+     * @param user New user data
+     * @return True if success, false if failed
+     */
+    bool editUser(const std::string& userName, const UserData& user);
+
+    /**
+     * @brief Search user in list
+     * @param userName Username of user to search
+     * @return Index of user in list if found, -1 if not found 
+     */
+    int searchUser(const std::string& userName) const;
+
+    /**
+     * @brief List all users
+     * @return List of users
+     */
+    std::vector<UserData> listUsers() const;
+
+    /**
+     * @brief Import user data from file
+     * @return True if success, false if failed
+     */
+    bool importUserData();
+};
+
+
+#endif //CTDL_GK_USERS_H
+
+#pragma clang diagnostic pop
